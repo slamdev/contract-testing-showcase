6058d2635bd284cacbf5c9ab2a7346b887dcdb8d
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2017-09-09 12:21:50 +0200

    add sample test based on cukes framework

---
+++ src/test/java/com/github/slamdev/contracts/TestRunner.java
@@ -0,0 +1,15 @@
+package com.github.slamdev.contracts;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {"pretty", "json:build/cucumber.json", "lv.ctco.cukes.core.formatter.CukesJsonFormatter:build/cucumber2.json"},
+        features = "classpath:features",
+        glue = "lv.ctco.cukes",
+        strict = true
+)
+public class TestRunner {
+}
